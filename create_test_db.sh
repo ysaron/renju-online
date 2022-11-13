b9c93a833231d3b0d9e2ed8e508ca1680c5322ac
ysaron <fr09sar0n@gmail.com>
2022-11-13 16:45:01 +0700

    Minor docker refactoring

--- create_test_db.sh
+++ create_test_db.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
 set -e
+
 test_db="${POSTGRES_DB}_tests";
+
 echo "Creating test database..."
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE $test_db;
 	GRANT ALL PRIVILEGES ON DATABASE $test_db TO "$POSTGRES_USER";
 EOSQL
+
 echo "Test database ${test_db} created."
